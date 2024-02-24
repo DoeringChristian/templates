76b3f7845127d4db8481508d8fcf4eb5f3df303f
Christian Döring <christian.doering@tum.de>
2024-02-24 16:19:14 +0100

    Added some test

---
+++ test/fhs/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      fhs = pkgs.buildFHSUserEnv {
+        name = "fhs-shell";
+        targetPkgs = pkgs: [
+          pkgs.cudatoolkit
+        ];
+      };
+    in
+    {
+      devShells.${system}.default = fhs.env;
+    };
+}
