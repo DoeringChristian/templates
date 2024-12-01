145c1213b19dce4a4dfab7a6fd6b9ddfcfd0f8ec
Christian Döring <christian.doering@tum.de>
2024-12-01 20:55:51 +0100

    Added lmt figures

--- lmtpresentation/{{ cookiecutter.__project_slug }}/.nvim.lua
+++
@@ -1,9 +0,0 @@
-UpdateLspSettings("texlab", function(settings)
-    settings.texlab.build = {
-        executable = "make",
-        args = {
-            "pdf"
-        },
-        onSave = true
-    }
-end)
